8b6e8d275d93d4598b9b5dd90889d6143861da43
KostyaSkorik <endatedd@student.21-school.ru>
2025-08-07 20:20:41 +1000

    Initial commit

---
+++ src/main/java/by/kostya/academy_crud_api/AcademyCrudApiApplication.java
@@ -0,0 +1,15 @@
+package by.kostya.academy_crud_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
+@SpringBootApplication
+public class AcademyCrudApiApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(AcademyCrudApiApplication.class, args);
+    }
+
+}
